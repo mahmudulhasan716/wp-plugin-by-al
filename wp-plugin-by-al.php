983525a0beafe5efc189a6594435348a1886329d
mahmudulhasan716 <mahmudul9944@gmail.com>
2025-09-19 10:09:06 +0600

    plugin init

---
+++ wp-plugin-by-al.php
@@ -0,0 +1,18 @@
+/**
+ * Plugin Name: WP Plugin by Mamun
+ * Plugin URI: https://yourwebsite.com
+ * Description: AI-powered plugin by Mamun
+ * Version: 1.0.0
+ * Author: Mahmun
+ * Author URI: https://yourwebsite.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-plugin-by-al
+ * Domain Path: /languages
+ * Requires at least: 5.0
+ * Tested up to: 6.4
+ * Requires PHP: 7.4
+ * WC requires at least: 5.0
+ * WC tested up to: 8.5
+ * WooCommerce: true
+ */
